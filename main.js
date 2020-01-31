6107935829537efe266bbe41f8d900ec9b3744e0
emackinnon1 <emackinnon100390@gmail.com>
2020-01-30 22:20:27 -0700

    Change hideBanner func and add handleNavClick func

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
-  const close = document.getElementById('close');
-  const welcomeBox = document.getElementById('welcome');
+  const body = document.querySelector('body');
+  const close = body.querySelector('.close');
+  const welcomeBox = body.querySelector('.welcome');
+  const navGroup = body.querySelector('.navgroup');
+  const navButtons = navGroup.querySelectorAll('.nav_butts img');
+  const main1 = body.querySelector('.main1');
+  const main2 = body.querySelector('.main2');
+  const elephant = body.querySelector('.el_img');
 
-  close.addEventListener('click', () => {
-    welcomeBox.style.display = 'none';
-  });
+
+  function hideBanner() {
+    welcomeBox.classList.add('hidden');
+  }
+
+  function clear() {
+    navButtons.forEach((button) => {
+      button.classList.remove('highlight');
+    });
+  }
+
+function handleNavClick(e) {
+  clear();
+  if (e.target.matches('img')) {
+    e.target.classList.add('highlight');
+  }
+}
+
+// add click listener to elephant with below function
+
+function toggleMainViews() {
+  if (main2.style.display = 'none') {
+    main1.style.display === 'initial';
+  } else if (main1.style.display = 'none') {
+    main2.style.display = 'initial';
+  }
+}
+
+navGroup.addEventListener('click', handleNavClick);
+close.addEventListener('click', hideBanner);
